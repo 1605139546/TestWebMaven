ef456ea4a97d66805686447be05564b64017e9e5
1605139546 <44915224+1605139546@users.noreply.github.com>
2019-04-21 22:00:14 +0800

    注释放开

--- src/main/java/com/yh/cn/submit/TestMain.java
+++ src/main/java/com/yh/cn/submit/TestMain.java
@@ -2,8 +2,5 @@ package com.yh.cn.submit;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println("1");
-        System.out.println("2");
     }
 }
