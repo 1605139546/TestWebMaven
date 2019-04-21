ff432b5b95a1717078602878fa4e415561a4e4c7
1605139546 <44915224+1605139546@users.noreply.github.com>
2019-04-21 22:01:57 +0800

    打印123个

--- src/main/java/com/yh/cn/submit/TestMain.java
+++ src/main/java/com/yh/cn/submit/TestMain.java
@@ -4,8 +4,5 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("2");
-        System.out.println("2");
     }
 }
