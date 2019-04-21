cf87316edbebca9cabbb28567d4a43677736e66c
1605139546 <44915224+1605139546@users.noreply.github.com>
2019-04-21 21:40:23 +0800

    注释放开

--- src/main/java/com/yh/cn/demo/Main.java
+++ src/main/java/com/yh/cn/demo/Main.java
@@ -1,20 +1,28 @@
 package com.yh.cn.demo;
 
+import lombok.var;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
 
 
     public static void main(String[] args) {
         //提交吧
-//        String format = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
-//        System.out.println(format);
-//        List<String> list = new ArrayList<>(Arrays.asList("123","456","789"));
+        String format = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+        System.out.println(format);
+        List<String> list = new ArrayList<>(Arrays.asList("123","456","789"));
 //        list.forEach((var x)-> System.out.println(x));
-//        list.forEach(System.out::println);
-//        var multiline = "13 \n" +
-//                "45\n" +
-//                "67\n";
+        list.forEach(System.out::println);
+        var multiline = "13 \n" +
+                "45\n" +
+                "67\n";
 //        multiline.lines().forEach(System.out::println);
-//        System.out.println(multiline);
+        System.out.println(multiline);
 
     }
 
