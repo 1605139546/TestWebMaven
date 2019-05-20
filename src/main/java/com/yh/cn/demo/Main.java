b97ad37c9215e7d1ace16e4570c12d935d3f9ac9
易煌 <1605139546@qq.com>
2019-05-20 21:51:41 +0800

    提交代码

--- src/main/java/com/yh/cn/demo/Main.java
+++ src/main/java/com/yh/cn/demo/Main.java
@@ -1,29 +1,45 @@
 package com.yh.cn.demo;
 
-import lombok.var;
-import org.apache.commons.lang3.time.DateFormatUtils;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+/**
+ * @author 16051
+ */
 public class Main {
 
 
     public static void main(String[] args) {
 
+        Student student = new Student(1, "张三", 20,"2019-05-17 12:00:19");
+        Student student2 = new Student(1, "张三2", 25,"2019-05-18 11:56:23");
+        Student student3 = new Student(1, "张三3", 27,"2019-05-17 16:00:09");
+        List<Student> list = new ArrayList<Student>() {{
+            add(student);
+            add(student2);
+            add(student3);
+        }};
+        try {
+            Collections.sort(list, new Comparator<Student>() {
+                @Override
+                public int compare(Student o1, Student o2) {
+                    try {
+                        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(o2.getCreateTime()).
+                                compareTo(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(o1.getCreateTime()));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    return 0;
+                }
+            });
+            System.out.println(list);
+        }catch (Exception e){
 
-//        String format = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
-//        System.out.println(format);
-//        List<String> list = new ArrayList<>(Arrays.asList("123","456","789"));
-////        list.forEach((var x)-> System.out.println(x));
-//        list.forEach(System.out::println);
-//        var multiline = "13 \n" +
-//                "45\n" +
-//                "67\n";
-////        multiline.lines().forEach(System.out::println);
-//        System.out.println(multiline);
+        }
 
     }
 
---
+++ src/main/java/com/yh/cn/demo/Student.java
@@ -0,0 +1,15 @@
+package com.yh.cn.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String createTime;
+}
